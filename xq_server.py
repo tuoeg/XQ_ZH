2d40719f30ba9460e9c375920e1092492661cb56
jifengtuo <49616374+jifengtuo@users.noreply.github.com>
2021-09-01 20:16:57 +0800

    Add files via upload
    服务端

---
+++ xq_server.py
@@ -0,0 +1,26 @@
+from flask import Flask,url_for,redirect
+import json
+
+
+
+
+app=Flask(__name__)
+@app.route('/index')
+def index():
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__=="__main__":
+    #进行爬虫并存入数据库
+    app.run()
